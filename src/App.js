ae59e8532a6e454c0f96288225c9b9857c6b172b
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-06-17 00:06:55 +0300

    lab-08

--- src/App.js
+++ src/App.js
@@ -1,101 +1,84 @@
-
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import AlertMsg from "./components/AlertMsg";
-import SearchForm from './components/SearchForm';
-import Map from './components/Map';
-import CityData from './components/CityData';
+import React, { Component } from "react";
+import axios from "axios";
+import SearchForm from "./components/SearchForm";
+import AlertMessage from "./components/AlertMessage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Map from "./components/Map";
+import CityData from "./components/CityData";
 import Weather from "./components/Weather";
 
+
 export class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = {
-      cityName: '',
-      cityData: {},
-      show: false,
-      alert: false,
-      error: '',
+    this.state={
+      mycityName : '',
+      cityData:{},
+      show:false,
+      alert:'',
+      error:'',
       lat:'',
-      lon:'',
-      weatherData:[]
+      lon:''
     }
   }
-  updateCityName = (e) => {
-    this.setState({
-      cityName: e.target.value,
-    })
-  }
-  getData = async (e) => {
+   updateCityStateName= (e) =>{
+     this.setState({
+      mycityName :e.target.value,
+     })
+    //  console.log(this.state)
+   }
+
+
+   getCityData = async (e) => {
     e.preventDefault();
-      await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.47bfe57b7ed998830cb39cf684cd82cf&q=${this.state.cityName}&format=json`).then(myApiRes=>{
-        this.setState({
-          CityData:myApiRes.data[0],
-          lat:myApiRes.data[0].lat,
-          lon:myApiRes.data[0].lon
+    await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.47bfe57b7ed998830cb39cf684cd82cf&q=${this.state.mycityName}&format=json`).then(locationRes => {
+      this.setState({
+        cityData: locationRes.data[0],
+        lat: locationRes.data[0].lat,
+        lon: locationRes.data[0].lon,
       });
-      axios.get(`http://localhost:8080/weather?lon=${this.state.lon}&lat=${this.state.lat}`).then(weatherRes =>{
-        this.setState({
-          weatherData: weatherRes.data,
-          show: true,
-          alert: false,
-
-        })
+        axios.get(`http://localhost:8080/weather?lon=${this.state.lon}&lat=${this.state.lat}`).then(weatherRes => {
+          this.setState({
+            weatherData: weatherRes.data,
+            show: true,
+            alert: false
+          })
+         
       });
+         
+      
     });
-        
 
-  }
+  };
+    
   render() {
     return (
-      <div style={{ textAlign: 'center' }}>
+      <div>
         {this.state.alert &&
-          <AlertMsg
+          <AlertMessage
             error={this.state.error}
           />
         }
-        <Container>
+        <SearchForm
+          getCityData={this.getCityData}
+          updateCityStateName={this.updateCityStateName}
+        />
+        {(this.state.show) &&
+          <>
+            <Map
+              cityData={this.state.cityData}
+            />
+            <CityData
+              cityData={this.state.cityData}
+            />
+             <Weather
+              weather={this.state.weatherData}
+            /> 
 
-          <Row>
-
-            <Col>
-              <SearchForm
-                getData={this.getData}
-                updateCityName={this.updateCityName}
-              />
-              {(this.state.show) &&
-                <>
-
-                  <Map
-                    CityData={this.state.CityData}
-
-                  />
-
-                  <CityData
-                    CityData={this.state.CityData}
-                  />
-
-                  <Weather weather={this.state.weatherData}
-                  />
-
-
-
-                </>
-              }
-            </Col>
-
-
-          </Row>
-        </Container>
-
-
+          </>
+        }
       </div>
     )
   }
-}
-
-export default App
+} 
+export default App;
--- src/components/CityData.js
+++ src/components/CityData.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class CityData extends Component {
-    render() {
-        return (
-            <div>
-                <p>`The City Name is :{this.props.CityData.display_name}`</p>
-                <p>`The lat : {this.props.CityData.lat}`</p>
-                <p>`The lon : {this.props.CityData.lon}`</p>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+        <div>
+            <p>
+                `the city name is {this.props.cityData.display_name}`
+            </p>
+            <p>
+                `the latitude : {this.props.cityData.lat}`
+            </p>
+            <p>
+                `the longitude : {this.props.cityData.lon}`
+            </p>
+        </div>
+    )
+}
 }
 
-export default CityData
+export default CityData;
