148c86070a5117a416db2e691fdae6a1b7b7c883
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-06-25 14:05:09 +0300

    lab-06 all req

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AlertMsg from "./components/AlertMsg";
+import SearchForm from './components/SearchForm';
+import Map from './components/Map';
+import CityData from './components/CityData';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+      cityData: {},
+      weatherData:[],
+      show: false,
+      alert: false,
+      error: ''
+    }
+  }
+  updateCityName = (e) => {
+    this.setState({
+      cityName: e.target.value,
+    })
+  }
+  getData = async (e) => {
+    e.preventDefault();
+    try {
+      //const myApiRes = await axios.get(`${process.env.REACT_APP_URL}/weather-data`);
+      const res = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.47bfe57b7ed998830cb39cf684cd82cf&q=${this.state.cityName}&format=json`);
+      console.log(res.data[0]);
+      this.setState({
+        CityData: res.data[0],
+        //weatherData: myApiRes.data.data,
+        show: true,
+        alert: false
+      })
+    } catch (error) {
+      this.setState({
+        error: error.message,
+        alert: true
+      })
+    }
+
+  }
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        {this.state.alert &&
+          <AlertMsg
+            error={this.state.error}
+          />
+        }
+        <Container>
+
+          <Row>
+
+            <Col>
+              <SearchForm
+                getData={this.getData}
+                updateCityName={this.updateCityName}
+              />
+              {(this.state.show) &&
+                <>
+
+                  <Map
+                    CityData={this.state.CityData}
+
+                  />
+
+                  <CityData
+                    CityData={this.state.CityData}
+                  />
+
+
+
+                </>
+              }
+            </Col>
+
+
+          </Row>
+        </Container>
+
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
